940a87bd32b78a8b6af402c6ddd983dfaf4e894c
TaigaMikami <mikamimikami60@gmail.com>
2018-06-26 16:33:42 +0900

    add diary

--- myapp/views.py
+++ myapp/views.py
@@ -2,6 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-  return HttpResponse("Hello worls")
+  context = {
+    'name': "Taiga"
+  }
+  return render(request, 'myapp/index.html', context)
 
+def about(request):
+  return render(request, 'myapp/about.html')
 
+def info(request):
+  return render(request, 'myapp/info.html')
--- practice/settings.py
+++ practice/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp',
+    'diary',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,9 @@ ROOT_URLCONF = 'practice.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+          os.path.normpath(os.path.join(BASE_DIR, "templates")),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+  os.path.normpath(os.path.join(BASE_DIR, "assets")),
+)
+
+
